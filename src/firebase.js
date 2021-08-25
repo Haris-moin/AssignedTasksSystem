ac7527c463b8ad672f6aa505b49eafb0b1aea62f
Haris-moin <harismoin48@gmail.com>
2021-08-25 17:20:03 +0500

    update...

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCn7scGc9gh4bkI6pHtRw8lOOhlfHaAnlQ",
+  authDomain: "assignedtasks-46b0c.firebaseapp.com",
+  projectId: "assignedtasks-46b0c",
+  storageBucket: "assignedtasks-46b0c.appspot.com",
+  messagingSenderId: "874797413905",
+  appId: "1:874797413905:web:c5b54dd48796b8c43923b0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
